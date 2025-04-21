57ae046ef314fb8ab83c3ea910dd2cd6abdf052e
pandari <pandarikuntun@gmail.com>
2025-04-21 15:43:13 +0530

    file modify

--- not_prime_count.py
+++ not_prime_count.py
@@ -1,17 +1,17 @@
-def prime(num):
+def notprime(num):
     if num<1:
         return False
     for i in range(2, int(num**0.5) + 1):
         if num%i ==0:
-            return False
-    return True
+            return True
+    return False
 num=int(input("enter last number in series: "))
 sum=0
 count=0
 for i in range (2,num+1):
-    if prime(i):
+    if notprime(i):
         print(i,end=",")
         sum +=i
-        count +=1
+        count += 1
 print("\n""sum of series :",sum)
-print("count of numbers in series:", count)
+print(count)
