9d4cfd0e6b8ec43b7319371d25394b2ef79790f3
pandari <pandarikuntun@gmail.com>
2025-04-25 21:04:43 +0530

    updated flems

--- flames.py
+++ flames.py
@@ -1,16 +1,31 @@
-def flemes():
-    name1=input()
-    name2=input()
+def flames():
+    name1=input("Enter your Name: ")
+    name2=input("Enter the your crush Name: ")
     name1=name1.replace(" ","").lower()
     name2=name2.replace(" ","").lower()
-    letters=list(name1+name2)
-    for letter in set(name1):
-        if letter in name2:
-            letters.remove(letter)
-            letters.remove(letter)
-            count=0
-            for letter in letters:
-                count+=1
-            print(count)
-flemes()
+    add=name1+name2
+    result = "".join([letter for letter in add if add.count(letter) == 1])
+    count=len(result)
+    flames=["F","L","A","M","E","s"]
+    while len(flames) > 1:
+        index = (count % len(flames)) - 1
+        if index >= 0:
+            flames = flames[index + 1:] + flames[:index]
+        else:
+            flames.pop()
+    return flames[0]
 
+result=flames()
+
+if result == "F":
+    print("Frinds")
+elif result == "L":
+    print("Love")
+elif result == "A":
+    print("Affection")
+elif result == "M":
+    print("Marriage")
+elif result == "E":
+    print("Enemy")
+else:
+    print("Sister")
