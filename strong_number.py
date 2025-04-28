ee1084de035cbe745ebb28900c6b11f7b6f9e0fb
pandari <pandarikuntun@gmail.com>
2025-04-28 16:10:53 +0530

    updated

--- strong_number.py
+++ strong_number.py
@@ -1,3 +1,13 @@
-
-        
-    
+def strong_number():
+    n=input("enter number: ")
+    sum=0
+    for i in n:
+        fact =1
+        for j in range(1,int(i)+1):
+            fact=fact*j
+        sum=sum+fact
+    if sum==int(n):
+        print("strong number")
+    else:
+        print("not strong number")
+strong_number()
